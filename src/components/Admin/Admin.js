ed308654aaeab771bdab549d4902b3e487ae6885
marcos-y <yoldimarcos@gmail.com>
2020-05-13 23:29:28 -0300

    boton mi tienda agregado

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -29,34 +29,18 @@ const Admin = () => {
 
             
             <div className="menu">
-                <Link to="/MiTienda">
-                    <Button variant="info" id="botonGrande" size="lg">Mi tienda</Button>
+                
+                <Link to="/MisPromos">
+                    <Button variant="info" id="botonGrande" size="lg">Mis promociones</Button>
                 </Link>
+                
                 <Link to="/MisProductos">
                     <Button variant="success" id="botonGrande" size="lg">Mis productos</Button>
                 </Link>
-                
 
-                <Navbar bg="light" variant="light" expand="lg" id="navb">
-                    <Link to="/">
-                        <Navbar.Brand ></Navbar.Brand>
-                    </Link>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Button variant="light" id="navb">Cerrar tienda</Button>
-                            <Button variant="light" id="navb">Copiar mensajes para clientes</Button>
-                            <Link to="/Banners" >
-                            <Button variant="light" id="navb">Banners</Button>
-                            </Link>
-                            <Link to="/MisPromos" >
-                            <Button variant="light" id="navb">Mis promociones</Button>
-                            </Link>
-                        </Nav>
-                        
-                    </Navbar.Collapse>
-                </Navbar>
-
+                <Link to="/MiTienda">
+                    <Button variant="dark" size="lg">Mi Tienda</Button>
+                </Link>
             </div>
 
         </>
